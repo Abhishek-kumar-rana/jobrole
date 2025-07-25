03ef460f6300576848bb8ff34dd8862f6334965f
Abhishek <lalanrana01000@gmail.com>
2025-07-25 23:08:56 +0530

    ss

--- app/api/jobs/[jobid]/apply/route.ts
+++ app/api/jobs/[jobid]/apply/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(
   req: Request,
-  { params }: { params: { jobid: string } }
+  { params }:{ params: { jobid: string } }
 ) {
   const session = await auth();
 
